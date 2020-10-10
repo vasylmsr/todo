201e4ff0dd9d535977a140d88a0de882e20b96ef
--list <vasymsr@gmail.com>
2020-10-10 15:14:16 +0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+yarn run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git add -A
+git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:vasylmsr/todo.git master:gh-pages
+
+cd -
